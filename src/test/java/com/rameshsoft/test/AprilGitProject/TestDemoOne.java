42dbfb20929297833abb5d4df2610cd107847f52
rameshsoft91456 <rameshsoft.cucumber1@gmail.com>
2022-04-17 11:11:11 +0530

    Added AprilGitBatchProject

---
+++ src/test/java/com/rameshsoft/test/AprilGitProject/TestDemoOne.java
@@ -0,0 +1,13 @@
+package com.rameshsoft.test.AprilGitProject;
+
+import org.testng.annotations.Test;
+
+public class TestDemoOne {
+
+	@Test
+	public void TestOne() {
+		System.out.println("TestONe executed successfully........");
+		
+	}
+
+}
